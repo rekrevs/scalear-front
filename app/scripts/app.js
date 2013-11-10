22eff479655866bcfc2b15c287df20007074748e
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2013-11-10 14:57:59 +0200

    some cleaning

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,32 +9,26 @@
   'ui.bootstrap.accordion',
   'ui.bootstrap.tabs',
   'ui.bootstrap.collapse',
-  'ui.bootstrap.transition', 
+  'ui.bootstrap.transition',
   'ui.bootstrap.datepicker',
   'ui.bootstrap.alert',
   'ui.sortable',
-  'ngDragDrop', 
-  'pasvaz.bindonce', 
+  'ngDragDrop',
+  'pasvaz.bindonce',
   'infinite-scroll',
   'xeditable',
   'ui.calendar'
-])
-
-  .constant('scalear_api', {host:"http://localhost:3000"})
+]).constant('scalear_api', {host:'http://localhost:3000'})
   .constant('headers', {withCredentials: true, 'X-Requested-With': 'XMLHttpRequest'})
   .value('$anchorScroll', angular.noop)
   .run(function(editableOptions) {
       editableOptions.theme = 'bs2';
-  })  
-
-  .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
-
-	
-    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');        
+}).config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+	$httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
     $httpProvider.defaults.withCredentials = true;
-	  $httpProvider.interceptors.push('ServerInterceptor');
-    
+	$httpProvider.interceptors.push('ServerInterceptor');
     $urlRouterProvider.otherwise('/');
+    
     $stateProvider
       .state('index', {
         url: '/',
@@ -42,42 +36,42 @@
         controller:'MainCtrl'
       })
       .state('course', {
-        url: "/courses/:course_id",
+        url: '/courses/:course_id',
         template: '<ui-view/>',
         abstract:true
       })
       .state('course.course_editor', {
-        url: "/course_editor",
+        url: '/course_editor',
         templateUrl: 'views/teacher/course_editor/course_editor.html',
         controller: 'courseEditorCtrl'
       })
       .state('course.course_editor.module',{
         resolve:{
           module:function($http, $stateParams, $rootScope, scalear_api, headers){
-            return $http({method: 'GET', headers:headers, url: scalear_api.host+'/en/courses/'+$stateParams.course_id+'/groups/'+$stateParams.module_id+'/get_group_angular'})
+            return $http({method: 'GET', headers:headers, url: scalear_api.host+'/en/courses/'+$stateParams.course_id+'/groups/'+$stateParams.module_id+'/get_group_angular'});
           }
         },
         url:'/modules/:module_id',
         views:{
-          "details" :{templateUrl: 'views/teacher/course_editor/module.details.html', controller: "moduleDetailsCtrl"},
-          "middle"  :{templateUrl: 'views/teacher/course_editor/module.middle.html',  controller: "moduleMiddleCtrl"}
+          'details' :{templateUrl: 'views/teacher/course_editor/module.details.html', controller: 'moduleDetailsCtrl'},
+          'middle'  :{templateUrl: 'views/teacher/course_editor/module.middle.html',  controller: 'moduleMiddleCtrl'}
         }
       })
-      .state('course.course_editor.lecture', { 
-        resolve:{ 
+      .state('course.course_editor.lecture', {
+        resolve:{
           lecture:function($http, $stateParams, $rootScope, scalear_api, headers){
             return $http({method: 'GET', headers:headers, url: scalear_api.host+'/en/courses/'+$stateParams.course_id+'/lectures/'+$stateParams.lecture_id})
           }
         },
-        url: "/lectures/:lecture_id",
+        url: '/lectures/:lecture_id',
         views:{
-          "details" :{templateUrl: 'views/teacher/course_editor/lecture.details.html', controller: "lectureDetailsCtrl"},
-          "middle"  :{templateUrl: 'views/teacher/course_editor/lecture.middle.html',  controller: "lectureMiddleCtrl"}
+          'details' :{templateUrl: 'views/teacher/course_editor/lecture.details.html', controller: 'lectureDetailsCtrl'},
+          'middle'  :{templateUrl: 'views/teacher/course_editor/lecture.middle.html',  controller: 'lectureMiddleCtrl'}
         }        
       })
       .state('course.course_editor.lecture.quizList', {
         views:{         
-          "quizList":{templateUrl: 'views/teacher/course_editor/lecture.middle.quiz_list.html', controller: "lectureQuizListCtrl"}
+          'quizList':{templateUrl: 'views/teacher/course_editor/lecture.middle.quiz_list.html', controller: 'lectureQuizListCtrl'}
         }        
       })
       .state('course.course_editor.quiz', {
@@ -86,17 +80,16 @@
             return $http({method: 'GET', url: scalear_api.host+'/en/courses/'+$stateParams.course_id+'/quizzes/'+$stateParams.quiz_id, headers: headers})
           }
         },
-        url: "/quizzes/:quiz_id",
+        url: '/quizzes/:quiz_id',
         views:{
-
-          "details" :{templateUrl: 'views/teacher/course_editor/quiz.details.html', controller: "quizDetailsCtrl"},
-          "middle"  :{templateUrl: 'views/teacher/course_editor/quiz.middle.html',  controller: "quizMiddleCtrl"}
+          'details' :{templateUrl: 'views/teacher/course_editor/quiz.details.html', controller: 'quizDetailsCtrl'},
+          'middle'  :{templateUrl: 'views/teacher/course_editor/quiz.middle.html',  controller: 'quizMiddleCtrl'}
         }
       })
       .state('course.calendar', {
         resolve:{
           events:function($http, $stateParams, headers,scalear_api){
-            console.log("resolveing")
+            console.log("resolving")
             return $http({method:'GET', url:scalear_api.host+'/en/courses/'+$stateParams.course_id+'/events', headers:headers})
           }
         },

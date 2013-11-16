fd7c7da9c781882faf7fc16f54e8e314ed8818f2
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2013-11-16 21:35:21 +0200

    teacher can now add, remove, edit teachers to a course

--- app/scripts/controllers/teacher/course/teachers.js
+++ app/scripts/controllers/teacher/course/teachers.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('scalearAngularApp')
-  .controller('TeacherCourseTeachersCtrl', ['$scope', '$http', 'Course', 'teachers', function ($scope, $http, Course, teachers) {
+  .controller('TeacherCourseTeachersCtrl', ['$scope', '$http', '$state', 'Course', 'teachers', function ($scope, $http, $state, Course, teachers) {
         $scope.teachers = teachers.data;
+        $scope.new_teachers = []
         console.log(teachers.data);
 
 //        $scope.rows[0] = "<td>"+$scope.teachers[0].email+"</td><td  style = \'width:500px\'><details-select value=\'" + $scope.teachers[0].role + "\' options=\'roles\' save=\'\' style=\'width: 120px;\'></details-select></td>";
@@ -30,19 +31,34 @@ angular.module('scalearAngularApp')
         $scope.roles = [{value:3, text:'Professor'}, {value:4, text:'TA'}];
 
         $scope.addRow = function(index){
-            $scope.teachers.splice(1, 0, {email: '', role : 'role', status:'pending'})
-            console.log($scope.teachers);
+            $scope.new_teachers.splice(index+1, 0, {email:'', role: '', status: 'pending'});
+//            $scope.teachers.splice(index+1, 0, {email: '', role : 'role', status:'pending'})
+            console.log($scope.new_teachers);
         }
+
         $scope.removeRow = function(index){
             var answer = confirm('Are you sure that you want to remove \''+$scope.teachers[index].email+'\' from this course?');
             if(answer){
-                Course.delete_teacher({email:$scope.teachers[index].email});
-                $scope.teachers.splice(index, 1);
+                Course.delete_teacher({email:$scope.teachers[index].email}).$promise.then(
+                    function(value) {$scope.teachers.splice(index, 1);},
+                    //handle the server error
+                    function(value) {}
+                )
+
                 console.log($scope.teachers);
             }
         }
         $scope.updateTeacher = function(index){
-            Course.save_teacher({email:$scope.teachers[index].email, role_id:$scope.teachers[index].role});
-
+            Course.update_teacher({email:$scope.teachers[index].email, role_id:$scope.teachers[index].role});
+        }
+        $scope.removeNewRow = function(index){
+            $scope.new_teachers.splice(index, 1);
+        }
+        $scope.saveTeachers = function(){
+            Course.save_teachers({new_teachers:$scope.new_teachers}).$promise.then(
+                function(value) {$scope.error = value.error},
+                function(value) {$scope.error = value}
+            )
+            //$state.go('course.teachers');
         }
   }]);

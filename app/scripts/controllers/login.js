5677db4ab4f36d5c7265e1c9ad66ad0fe149c71a
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-01-16 17:28:34 +0200

    finished translations for authentication views

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,7 +3,9 @@
 angular.module('scalearAngularApp')
   .controller('LoginCtrl',['$scope','scalear_api','$location','$log', '$translate', function ($scope, scalear_api, $location, $log, $translate, redirection_url) {
    $scope.login = function(){
-   	$log.debug("in login");
+//   	$log.debug("in login");
+       console.log('here');
+       $scope.singleModel = 1;
 //    window.location=scalear_api.host+"/"+$scope.current_lang+"/users/sign_angular_in?angular_redirect="+scalear_api.redirection_url; //http://localhost:9000/#/ //http://angular-edu.herokuapp.com/#/
    }
    

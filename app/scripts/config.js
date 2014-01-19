51a72c575343a0f4daac2b36b9d234f9786c6096
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-01-19 13:23:25 +0200

    modified authentication views & home page redirects to login page

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,8 +3,9 @@
  angular.module("config", [])
 
 .constant("scalear_api", {
-  "host": "",
-  "redirection_url": ""
+  "host": "http://localhost:3000",
+  "redirection_url": "http://localhost:3000/#/",
+  "version": "2.0.5"
 })
 
 ;
--- app/scripts/directives/footer_directives.js
+++ app/scripts/directives/footer_directives.js
@@ -1,49 +1,50 @@
 'use strict';
 
 angular.module('scalearAngularApp')
-.directive('scalearFooter', function() {
-	return{
-		restrict: 'E',
-		template:'<div class="row"><div class="span12" >'+
-                    '<center>'+ 
-                        '&copy;2013 ScalableLearning | '+ 
-                        '<span translate="footer.about"></span> | '+ 
-                        '<a ui-sref="privacy" translate="footer.privacy"></a> '+
-                        '<report_technical ng-show="current_user"/>'+                             
-                    '</center>'+
+    .directive('scalearFooter', function(scalear_api) {
+        return {
+            restrict: 'E',
+            template: '<div class="row"><div class="span12" >' +
+                '<center>' +
+                '&copy;2013 ScalableLearning v' + scalear_api.version + '| ' +
+                '<span translate="footer.about"></span> | ' +
+                '<a ui-sref="privacy" translate="footer.privacy"></a> ' +
+                '<report_technical ng-show="current_user"/>' +
+                '</center>' +
                 '</div>'
-	};
-})
-.directive('reportTechnical', ['Home', '$location','$log',function(Home, $location, $log) {
-	return{
-		restrict: 'E',
-		template:'| <a style="cursor:pointer;" ng-click="toggleTechnicalDisplay()" translate="feedback.report_technical"></a>'+
-				 '<div ng-show="show_technical">'+
-					'<textarea rows="3" cols="10" style="width:400px;" ng-model="technical_data"></textarea><br />'+
-					'<a style="cursor:pointer;" ng-click="send_technical()" translate="feedback.send"></a>'+
-					'<loading size="small" show="sending_technical"/>'+ 
-				 '</div>',
-		link: function(scope, element){
-			scope.show_technical = false
-			scope.toggleTechnicalDisplay=function(){
-				scope.show_technical= !scope.show_technical
-			}
-			scope.send_technical = function(){
-		  		$log.debug("in sending");
-		  		scope.sending_technical=true;
-		  		Home.technicalProblem(
-		  			{
-		  				url:$location.url(), 
-		  				problem:scope.technical_data, 
-		  				lang: scope.current_lang
-		  			},
-		  			function(data){
-			  			scope.technical_data="";
-			  			scope.sending_technical=false;
-			  			scope.show_technical=false;
-			  		}
-		  		);
-		  	};
-		}
-	};
-}])
+        };
+    })
+    .directive('reportTechnical', ['Home', '$location', '$log',
+        function(Home, $location, $log) {
+            return {
+                restrict: 'E',
+                template: '| <a style="cursor:pointer;" ng-click="toggleTechnicalDisplay()" translate="feedback.report_technical"></a>' +
+                    '<div ng-show="show_technical">' +
+                    '<textarea rows="3" cols="10" style="width:400px;" ng-model="technical_data"></textarea><br />' +
+                    '<a style="cursor:pointer;" ng-click="send_technical()" translate="feedback.send"></a>' +
+                    '<loading size="small" show="sending_technical"/>' +
+                    '</div>',
+                link: function(scope, element) {
+                    scope.show_technical = false
+                    scope.toggleTechnicalDisplay = function() {
+                        scope.show_technical = !scope.show_technical
+                    }
+                    scope.send_technical = function() {
+                        $log.debug("in sending");
+                        scope.sending_technical = true;
+                        Home.technicalProblem({
+                                url: $location.url(),
+                                problem: scope.technical_data,
+                                lang: scope.current_lang
+                            },
+                            function(data) {
+                                scope.technical_data = "";
+                                scope.sending_technical = false;
+                                scope.show_technical = false;
+                            }
+                        );
+                    };
+                }
+            };
+        }
+    ])

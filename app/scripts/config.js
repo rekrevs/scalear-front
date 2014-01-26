d198ffea722923c7f2659baaea42019dd09926c5
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2014-01-26 14:23:12 +0200

    issues 331 and 324

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://localhost:3000",
   "redirection_url": "http://localhost:3000/#/",
-  "version": "2.0.10 (Mon, 20 Jan 2014 12:08:58 GMT)"
+  "version": "2.0.12 (Sun, 26 Jan 2014 09:31:28 GMT)"
 })
 
 ;

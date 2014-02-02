e8c8d207866ba984c6c5fe0b29045389b39a3dac
Karim Alaa <karim.hamed89@gmail.com>
2014-02-02 17:01:32 +0200

    remove active_users from statistics view

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,9 +3,9 @@
  angular.module("config", [])
 
 .constant("scalear_api", {
-  "host": "",
-  "redirection_url": "",
-  "version": "2.1.1 (Thu, 30 Jan 2014 14:37:29 GMT)"
+  "host": "http://localhost:3000",
+  "redirection_url": "http://localhost:3000/#/",
+  "version": "2.1.1 (Sun, 02 Feb 2014 14:57:39 GMT)"
 })
 
 ;

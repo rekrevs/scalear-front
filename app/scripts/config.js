8eca3fae051f2a454273c415add457574edbdf78
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2014-02-06 09:06:44 +0200

    Course list for teachers updated to include enrollments instead of users. (faster)

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://localhost:3000",
   "redirection_url": "http://localhost:3000/#/",
-  "version": "2.1.3 (Wed, 05 Feb 2014 08:25:32 GMT)"
+  "version": "2.1.3 (Wed, 05 Feb 2014 12:26:31 GMT)"
 })
 
 ;

99dadc89975c2d0f6ce0b9b2cb1c3929160797bd
Karim Alaa <karim.hamed89@gmail.com>
2014-02-11 10:26:03 +0200

    adjusted configuration files

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://localhost:3000",
   "redirection_url": "http://localhost:3000/#/",
-  "version": "2.1.5 (Mon, 10 Feb 2014 10:09:15 GMT)"
+  "version": "2.1.6 (Tue, 11 Feb 2014 08:07:36 GMT)"
 })
 
 ;

8cf05e6fbac20ed2c68f53288ad7c5ed5a06ac97
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2014-02-21 12:58:03 +0200

    adding votes and flagging to forum

--- app/scripts/services/forum.js
+++ app/scripts/services/forum.js
@@ -6,7 +6,10 @@ angular.module('scalearAngularApp')
     $http.defaults.useXDomain = true;
     return $resource(scalear_api.host+'/:lang/discussions/:action', {lang:$translate.uses()},
       {
-        'createComment':{method: 'POST', params: {action: 'create_comment'}, headers: headers}
+        'createComment':{method: 'POST', params: {action: 'create_comment'}, headers: headers},
+         'getComments':{method: 'GET', isArray: true, params: {action: 'get_comments'}, headers: headers},
+          'vote':{method: 'POST', params: {action: 'vote'}, headers: headers},
+          'flag':{method: 'POST', params: {action: 'flag'}, headers: headers}
       });
 
 }]);

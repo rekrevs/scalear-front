6498ad4baac947fe02ed141acfd32579b78f8979
Karim Alaa <karim.hamed89@gmail.com>
2014-03-17 09:45:21 +0200

    SCAL-436 | Make timezone menu easier to use

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://localhost:3000",
   "redirection_url": "http://localhost:3000/#/",
-  "version": "2.1.10 (Mon, 10 Mar 2014 12:06:19 GMT)",
+  "version": "2.1.12 (Sun, 16 Mar 2014 13:14:37 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

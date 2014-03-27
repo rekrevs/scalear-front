2b2c563d9445d6bb5fb459e0f1045913b273607c
Karim Alaa <karim.hamed89@gmail.com>
2014-03-27 09:48:33 +0200

    fixed issue with css

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "2.1.10 (Mon, 24 Mar 2014 07:56:06 GMT)",
+  "version": "2.1.10 (Thu, 27 Mar 2014 07:46:33 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

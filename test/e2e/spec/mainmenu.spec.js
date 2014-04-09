5c8d4354b4c53710c0a53a3a665bb4382d07e981
menahappy <mena.happy@yahoo.com>
2014-04-09 11:50:27 +0200

    added some more tests

--- test/e2e/spec/mainmenu.spec.js
+++ test/e2e/spec/mainmenu.spec.js
@@ -20,9 +20,48 @@ describe('', function(){
 	it('should login', function(){
 		functions.sign_in(ptor, mail, password, functions.feedback)
 	})
+	it('should open course', function(){
+		//open_course(ptor);
+	})
 	it('should open tray', function(){
-		functions.open_tray(ptor).then(function(){
-			console.log('eshta');
-		})
+		functions.open_tray(ptor);
+	})
+	it('item are visible in the course page',function(){
+		//check_for_menu_icons_true(ptor);
+		check_for_nav_bar_icons_true(ptor);
 	})
 })
+
+open_course = function(ptor){
+	ptor.findElements(protractor.By.partialLinkText('|')).then(function(course){
+		course[0].click();
+	})
+}
+
+check_for_menu_icons_true = function(ptor){
+
+	info_icon = ptor.findElement(protractor.By.id("info"));
+    expect(info_icon.isDisplayed()).toEqual(true);
+
+    info_icon = ptor.findElement(protractor.By.id("calendar"));
+    expect(info_icon.isDisplayed()).toEqual(true);
+
+    info_icon = ptor.findElement(protractor.By.id("lectures"));
+    expect(info_icon.isDisplayed()).toEqual(true);
+}
+
+check_for_nav_bar_icons_true = function(ptor){
+
+	settings_btn = ptor.findElement(protractor.By.id("settings_btn"));
+    expect(settings_btn.isDisplayed()).toEqual(true);
+
+    language_link_sweden = ptor.findElement(protractor.By.id("language_link_sweden"));
+    expect(language_link_sweden.isDisplayed()).toEqual(true);
+
+    help_link = ptor.findElement(protractor.By.id("help_link"));
+    expect(help_link.isDisplayed()).toEqual(true);
+
+    logout_link = ptor.findElement(protractor.By.id("logout_link"));
+    expect(logout_link.isDisplayed()).toEqual(true);
+}
+

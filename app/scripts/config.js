f59d2bb8df794fd2ecafb5623ae6de26b203d498
Karim Alaa <karim.hamed89@gmail.com>
2014-04-16 18:05:30 +0200

    fixed other news feed missing images

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "",
   "redirection_url": "",
-  "version": "2.7.0 (Wed, 16 Apr 2014 14:26:16 GMT)",
+  "version": "2.7.0 (Wed, 16 Apr 2014 16:04:02 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

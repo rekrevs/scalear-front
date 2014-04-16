1c687c3c4aaf782b3a5bc0c3b17df2c94cca695c
Karim Alaa <karim.hamed89@gmail.com>
2014-04-16 15:29:19 +0200

    fixed announcement css

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "",
   "redirection_url": "",
-  "version": "2.7.0 (Wed, 16 Apr 2014 13:21:00 GMT)",
+  "version": "2.7.0 (Wed, 16 Apr 2014 13:29:03 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

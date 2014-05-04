3fc843b8aa7a1343dd788f6490306fbca80335a5
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-05-04 09:58:43 +0200

    fixed the layout for the preview button in course editor

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "2.7.0 (Thu, 17 Apr 2014 08:22:35 GMT)",
+  "version": "2.7.0 (Sun, 04 May 2014 07:53:42 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

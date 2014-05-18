6be9238f51d6b37bd15aa3303256edccec5a7704
Karim Alaa <karim.hamed89@gmail.com>
2014-05-18 12:05:25 +0200

    Fixed: - issue with changing languages not working - issue when creating new courses

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "",
   "redirection_url": "",
-  "version": "2.10 (Sun, 18 May 2014 09:39:22 GMT)",
+  "version": "2.10.1 (Sun, 18 May 2014 10:03:50 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 
--- app/scripts/controllers/teacher/progress/progress_lecture_ctrl.js
+++ app/scripts/controllers/teacher/progress/progress_lecture_ctrl.js
@@ -19,14 +19,14 @@ angular.module('scalearAngularApp')
       removeShortcuts()
     });
 
-    // $scope.filters=
-    // {
-    //   $translate("all"):"",
-    //   $translate("lecture.confused"):"confused",
-    //   // "Questions":"question",
-    //   $translate("courses.charts"): "charts",
-    //   $translate("lecture.discussion"): "discussion",
-    // }
+    $scope.filters=
+    {
+      "all":"",
+      "lectures.confused":"confused",
+      // "Questions":"question",
+      "courses.charts": "charts",
+      "lectures.discussion": "discussion",
+    }
 
     $scope.grade_options= [{
       value: 0, // not set

79ed4a2fdb7b3612a365842edc215b1ed62cc28c
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-05-18 11:11:26 +0200

    changes to help in testing

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,9 +3,9 @@
  angular.module("config", [])
 
 .constant("scalear_api", {
-  "host": "",
-  "redirection_url": "",
-  "version": "2.10 (Sun, 18 May 2014 09:39:22 GMT)",
+  "host": "http://0.0.0.0:3000",
+  "redirection_url": "http://0.0.0.0:3000/#/",
+  "version": "2.10 (Sun, 18 May 2014 09:04:38 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

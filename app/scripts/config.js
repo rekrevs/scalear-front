169a2fb8b5f433a76d812c7f9339574c3012a9a2
Karim Alaa <karim.hamed89@gmail.com>
2014-05-28 16:48:11 +0200

    Fixed: - issue with statistics css page for staging - added shortcuts panel for progress pages - changed in class time estimate to be less bold

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "",
   "redirection_url": "",
-  "version": "2.11.3 (Wed, 28 May 2014 10:17:36 GMT)",
+  "version": "2.11.3 (Wed, 28 May 2014 14:45:33 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

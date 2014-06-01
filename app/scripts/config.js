2b76fa61fce8ae660df38ba1a2f58dd89192cd3c
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-06-01 11:48:09 +0200

    added a select module/item notice when the user accesses the course editor, and modified student timeline to solve the scrolling issues.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "2.11.5 (Sat, 31 May 2014 20:38:38 GMT)",
+  "version": "2.11.5 (Sun, 01 Jun 2014 09:06:55 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

9bc78299454e4ef50815cdbc86a444800395f194
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-08-03 09:38:40 +0300

    finished the feature

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.0 (Sun, 27 Jul 2014 14:47:19 GMT)",
+  "version": "3.0 (Sun, 03 Aug 2014 05:41:26 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

7087d26a820be8bdb7431a632e56c48f68b40d54
Karim Alaa <karim.hamed89@gmail.com>
2014-08-13 15:37:28 +0300

    Bumped to V3.5

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,9 +3,9 @@
  angular.module("config", [])
 
 .constant("scalear_api", {
-  "host": "http://0.0.0.0:3000",
-  "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.4.2 (Wed, 13 Aug 2014 08:18:30 GMT)",
+  "host": "",
+  "redirection_url": "",
+  "version": "3.5 (Wed, 13 Aug 2014 12:37:03 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

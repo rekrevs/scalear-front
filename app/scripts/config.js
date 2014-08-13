57e8664a5e239685abe2cdfa6196494dfc2c3e5f
Karim Alaa <karim.hamed89@gmail.com>
2014-08-13 18:49:54 +0300

    fixed issue in home page

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "",
   "redirection_url": "",
-  "version": "3.5.1 (Wed, 13 Aug 2014 15:38:58 GMT)",
+  "version": "3.5.1 (Wed, 13 Aug 2014 15:48:23 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

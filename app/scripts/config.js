4d996bc68dc6cb78fc6ef9e860c14a81b5ef19ee
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-08-13 11:53:51 +0300

    moved graded/required labels to be inside the quiz

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -3,9 +3,9 @@
  angular.module("config", [])
 
 .constant("scalear_api", {
-  "host": "",
-  "redirection_url": "",
-  "version": "3.4.2 (Tue, 12 Aug 2014 13:44:10 GMT)",
+  "host": "http://0.0.0.0:3000",
+  "redirection_url": "http://0.0.0.0:3000/#/",
+  "version": "3.4.2 (Wed, 13 Aug 2014 08:18:30 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

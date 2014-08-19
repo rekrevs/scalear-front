014baed35fdf709ec3bbd61448510a754ff4d79f
Karim Alaa <karim.hamed89@gmail.com>
2014-08-19 14:31:17 +0300

    fixed issue with content navigator pushing content to the left bumped to V3.6.1

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.6 (Mon, 18 Aug 2014 17:36:12 GMT)",
+  "version": "3.6.1 (Tue, 19 Aug 2014 11:28:07 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

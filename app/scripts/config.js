16b1bf831f3e01da048956645892ac5e92b0e5ad
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-08-19 11:03:10 +0300

    added moment to bower.son

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.5.1 (Sun, 17 Aug 2014 07:28:38 GMT)",
+  "version": "3.6 (Mon, 18 Aug 2014 17:36:12 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

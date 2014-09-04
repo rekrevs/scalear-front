c77fda8d706d879021d5311545bfa7b71d9d29ea
Karim Alaa <karim.hamed89@gmail.com>
2014-09-04 14:57:04 +0300

    Fixes: - added smooth scrolling to student timeline - issue where click on course title twice goes to empty page - separated views for drag html quizzes to handle images for grunt - issue with saving notes - issue with deleting confused - handling content navigator opening with video on different scenarios

--- app/scripts/controllers/student/lectures/lectures_middle_ctrl.js
+++ app/scripts/controllers/student/lectures/lectures_middle_ctrl.js
@@ -24,7 +24,9 @@ angular.module('scalearAngularApp')
 
     $scope.$on('lecture_filter_update',function(ev,filters){
       $scope.checkModel=filters
-      $scope.scrollIntoView('outline')
+      $timeout(function(){
+        $scope.scrollIntoView()
+    },200)
     })
 
     $scope.$on('content_navigator_change',function(ev, status){
@@ -84,9 +86,6 @@ angular.module('scalearAngularApp')
         $scope.$watch('timeline',function(){
             if($scope.timeline){
                 goToLecture($state.params.lecture_id) 
-                $timeout(function(){
-                    $scope.scrollIntoView('outline')
-                },500)
             }
         })
 
@@ -157,6 +156,9 @@ angular.module('scalearAngularApp')
                 if(isiPad()){
                     $scope.video_ready=true
                 }
+                $timeout(function(){
+                    $scope.scrollIntoView()
+                },500)
             })
         }
     }
@@ -202,12 +204,14 @@ angular.module('scalearAngularApp')
             $scope.seek(parseInt(time));
     }
 
-    $scope.scrollIntoView=function(tab, fast){
+    $scope.scrollIntoView=function(){
+        console.log("scroll to view")
         if($scope.lecture && !isiPad()){
-            $timeout(function(){
-                $location.hash(tab+'_'+$scope.lecture.id);
-                $anchorScroll();    
-            })
+            $('.student_timeline').scrollTo('#outline_'+$scope.lecture.id, {offsetTop: 100, duration: 400});
+            // $timeout(function(){
+            //     $location.hash(tab+'_'+$scope.lecture.id);
+            //     $anchorScroll();    
+            // })
         }
     }
 

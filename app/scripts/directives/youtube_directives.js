e4ded24b2c81f7bb9d2ee8492869022df274ac9f
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-09-09 16:58:13 +0300

    teacher side squishing done

--- app/scripts/directives/youtube_directives.js
+++ app/scripts/directives/youtube_directives.js
@@ -31,11 +31,12 @@ angular.module('scalearAngularApp')
 			transclude: true,
 			replace:true,
 			restrict: "E",
-			template: '<div class="videoborder panel widescreen" style="padding:0; border:none" ng-transclude></div>' //style="border:4px solid" 
+			template: '<div class="videoborder panel widescreen transparent" style="padding:0; border:none" ng-transclude></div>' //style="border:4px solid" 
 		};
 	})
 	.directive('youtube',['$rootScope','$log','$timeout','$window',function($rootScope,$log,$timeout,$window){
 		return {
+			transclude: true,
 			restrict: 'E',
 			replace:true,
 			scope:{
@@ -47,7 +48,7 @@ angular.module('scalearAngularApp')
                 controls: '@',
 
 			},
-            template:"<div></div>",
+            template:"<div><div ng-transclude></div></div>",
 			link: function(scope, element){
 
                 scope.vq='hd720';

9183c953521edc6306c274206304e43bc8ebf5fe
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-09-09 22:50:15 +0300

    limited content navigator width to 300px

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.6.10 (Tue, 09 Sep 2014 09:53:27 GMT)",
+  "version": "3.6.11 (Tue, 09 Sep 2014 19:16:47 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

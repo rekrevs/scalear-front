c3c26328d7770a39aef2357c375d141299270106
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-09-10 17:29:22 +0300

    small fix for ontop layer for students

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.6.12 (Wed, 10 Sep 2014 13:20:12 GMT)",
+  "version": "3.6.13 (Wed, 10 Sep 2014 14:06:41 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

5805063381a8f0a5987a8efe316dfd6d38324e45
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-09-10 13:56:38 +0300

    removed left offset for module middle page under course editor when the content navigator is opened

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.6.11 (Tue, 09 Sep 2014 19:16:47 GMT)",
+  "version": "3.6.12 (Wed, 10 Sep 2014 10:38:35 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

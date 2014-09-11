2f182c121950d75d5e0a9d11ea5ece8c50e7ffc5
Mahmoud Menshawi <mahmoud.menshawi@acu.edu.eg>
2014-09-11 11:06:31 +0300

    hidden video time when in small screen

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,7 +5,7 @@
 .constant("scalear_api", {
   "host": "http://0.0.0.0:3000",
   "redirection_url": "http://0.0.0.0:3000/#/",
-  "version": "3.6.13 (Wed, 10 Sep 2014 14:06:41 GMT)",
+  "version": "3.6.14 (Thu, 11 Sep 2014 07:10:59 GMT)",
   "help_link": "http://www.it.uu.se/katalog/davbl791/scalable-learning-manual.pdf"
 })
 

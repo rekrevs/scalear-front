766475f1e6e78b0f99bf35623a5fd66d192423e1
Karim Alaa <karim.hamed89@gmail.com>
2016-02-17 12:30:34 +0200

    fixed issue where delete button not working

--- app/scripts/controllers/users/confirm_delete_ctrl.js
+++ app/scripts/controllers/users/confirm_delete_ctrl.js
@@ -8,16 +8,16 @@ angular.module('scalearAngularApp')
 	$scope.form={}
   $scope.user = user_new	
   $scope.ok = function () {
-  	if($scope.form.key.$valid){
-  	$scope.form.processing=true;
+  	if($scope.user.saml || $scope.form.key.$valid){
+    	$scope.form.processing=true;
       User.delete_account({password:$scope.enrollment.key}, {}, function() {
           $rootScope.current_user = null;
           $scope.form.processing=false;
           $modalInstance.close();
           $state.go('login')
       }, function(response) {
-          $scope.form.processing=false;
-          $scope.form.server_error=response.data.errors.join();
+        $scope.form.processing=false;
+        $scope.form.server_error=response.data.errors.join();
       })  		
   	}
     else

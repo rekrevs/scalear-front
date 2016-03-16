b38b2b732f6c1f995dc7103814d80d04220024dd
Karim Alaa <karim.hamed89@gmail.com>
2016-03-16 18:58:10 +0200

    Fixed: - issue where autocomplete can show irrelevent results - issue where pressing enter on a empty search result set can lead to an error Added a warning message if the provider failed to be reached

--- app/scripts/controllers/login_ctrl.js
+++ app/scripts/controllers/login_ctrl.js
@@ -2,7 +2,7 @@
 
 angular.module('scalearAngularApp')
   .controller('LoginCtrl',['$state','$scope','$rootScope', 'scalear_api','$window','$log', '$translate', 'User', 'Page', 'ErrorHandler','ngDialog','MobileDetector','Saml','$location','SWAMID','$cookieStore','$timeout', function ($state, $scope, $rootScope,scalear_api, $window, $log, $translate, User, Page, ErrorHandler,ngDialog, MobileDetector, Saml, $location, SWAMID, $cookieStore, $timeout) {
-  
+
   $scope.user={}
   Page.setTitle('navigation.login')
   $('#user_email').select()
@@ -35,31 +35,33 @@ angular.module('scalearAngularApp')
       $("#search").select()
     })
     shortcut.add("Enter", function(){
-      $scope.samlLogin($scope.swamid[0])
-    }) 
+      if($scope.swamid[0])
+        $scope.samlLogin($scope.swamid[0])
+
+    })
   }
 
   $scope.hideProviders=function(){
     $scope.show_providers=false
     shortcut.remove("Enter", function(){
       $scope.samlLogin($scope.swamid[0])
-    }) 
+    })
   }
 
   $scope.showLoginForm=function(){
     $scope.show_scalable_login=!$scope.show_scalable_login
     $scope.hideProviders()
   }
-  
+
   $scope.login = function(){
     $scope.sending = true;
     User.signIn({},
-      {"user":$scope.user}, 
+      {"user":$scope.user},
       function(data){
         $cookieStore.put('login_provider', 'scalablelearning')
         $log.debug("login success")
         $scope.sending = false;
-        $rootScope.$broadcast("get_current_courses") 
+        $rootScope.$broadcast("get_current_courses")
         $scope.is_mobile= MobileDetector.isMobile()
         if( $scope.is_mobile && ((data.roles[0].id != 2 && MobileDetector.isTablet()) ||  MobileDetector.isPhone)){
           ngDialog.open({
@@ -70,14 +72,14 @@ angular.module('scalearAngularApp')
             }
           });
         }
-        else        
+        else
           next(data)
       },function(){
         $scope.sending = false;
     });
   }
 
-  
+
 
   $scope.join=function(){
     $state.go("signup",  { input1 : $scope.user.email, input2: $scope.user.password})
@@ -100,9 +102,14 @@ angular.module('scalearAngularApp')
       {idp: idp.entityID},
       function(resp){
          $(resp.saml_url).appendTo('body').submit();
-      }, 
+      },
       function(){
-
+        $rootScope.busy_loading = false;
+        $rootScope.show_alert = "error";
+        ErrorHandler.showMessage('Could not reach provider', 'errorMessage', 8000);
+        $timeout(function() {
+            $rootScope.show_alert = "";
+        }, 4000);
       }
     )
   }

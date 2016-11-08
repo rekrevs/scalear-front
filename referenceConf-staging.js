70076afcd2ec840727ca2108bbe2972c20cc4e87
Usama Saied <usamaelqastaui@gmail.com>
2016-11-08 09:44:01 +0200

    last commit

--- referenceConf-staging.js
+++ referenceConf-staging.js
@@ -213,7 +213,7 @@ exports.config = {
         // 'test/e2e/spec/create_course.spec.js', // done(11/16)
         // 'test/e2e/spec/fill_course.spec.js', // done(11/16)
         // 'test/e2e/spec/course_information_validation.spec.js', // done(11/16)
-        // 'test/e2e/spec/account_information_validation.spec.js', // done(11/16)(@email.com is considered student) (teacher 1 data were changed in the conf-stag file.)
+        // 'test/e2e/spec/account_information_validation.spec.js', // done(11/16)
         // 'test/e2e/spec/enrollment_help.spec.js', // done(11/16)
         // 'test/e2e/spec/course_editor_basic.spec.js', // done(11/16)
         // 'test/e2e/spec/course_editor_copy.spec.js', // done(11/16)
@@ -223,11 +223,11 @@ exports.config = {
         // 'test/e2e/spec/teacher-managment.spec.js', // done(11/16)
         // 'test/e2e/spec/students_solve_course.spec.js', // done(11/16) (inOrder Functionality & explanation targeting)
         // 'test/e2e/spec/notes.spec.js', // done(11/16)
-        'test/e2e/spec/discussions.spec.js', //
+        // 'test/e2e/spec/discussions.spec.js', //
         // 'test/e2e/spec/progress-module.spec.js', //
         // 'test/e2e/spec/progress-completion-module.spec.js', //
         // 'test/e2e/spec/validations.spec.js', //
-        // 'test/e2e/spec/delete_course.spec.js', // 
+        // 'test/e2e/spec/delete_course.spec.js', //
 
 
         //rewritten tests
@@ -239,7 +239,7 @@ exports.config = {
 
         // 'test/e2e/spec/inclass_pi.spec.js', //(refresh problem)
 
-        // 'test/e2e/spec/fill_course_dp.spec.js',
+        // 'test/e2e/spec/fill_course_dp.spec.js', (demo)
 
 
 

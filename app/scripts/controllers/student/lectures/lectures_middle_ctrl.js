a201dd650bf29d71dcf80ef072dc1cd013b9a30e
Ahmed Okasha <okashoon@gmail.com>
2018-01-02 18:11:23 +0200

    remove commented code of distance peer

--- app/scripts/controllers/student/lectures/lectures_middle_ctrl.js
+++ app/scripts/controllers/student/lectures/lectures_middle_ctrl.js
@@ -49,18 +49,18 @@ angular.module('scalearAngularApp')
     $scope.$watch('distance_peer_session_id', function(newval, oldval) {
       if ($scope.distance_peer_session_id && $scope.lecture != null) {
         if (!$scope.distance_peer_status) {
-          // Lecture.checkIfInDistancePeerSession({
-          //     course_id: $scope.lecture.course_id,
-          //     lecture_id: $scope.lecture.id
-          //   }).$promise
-          //   .then(function(response) {
-          //     if (response.distance_peer != "no_peer_session") {
-          //       $scope.name = response.name
-          //       $scope.distance_peer_status = response.user_distance_peer.status
-          //       $scope.distance_peer_message_in_box = $translate.instant("distance_peer.message_video", { name: $scope.name })
-          //       $scope.lecture_player.events.onReady(true)
-          //     }
-          //   })
+          Lecture.checkIfInDistancePeerSession({
+              course_id: $scope.lecture.course_id,
+              lecture_id: $scope.lecture.id
+            }).$promise
+            .then(function(response) {
+              if (response.distance_peer != "no_peer_session") {
+                $scope.name = response.name
+                $scope.distance_peer_status = response.user_distance_peer.status
+                $scope.distance_peer_message_in_box = $translate.instant("distance_peer.message_video", { name: $scope.name })
+                $scope.lecture_player.events.onReady(true)
+              }
+            })
         }
       }
     });
@@ -415,8 +415,8 @@ angular.module('scalearAngularApp')
       for (var each_30_second = 30; each_30_second < $scope.total_duration; each_30_second = each_30_second + 30) {
         duration_milestones.push(((each_30_second / $scope.total_duration) * 100))
       }
-      // waiting for peer session
-      // checkPeerSession().then(function() {
+      
+      checkPeerSession().then(function() {
         if (!close_student && $scope.lecture.distance_peer && !$scope.distance_peer_session_id) {
           $scope.openStudentList($scope.lecture.id, $scope.lecture.course_id)
         }
@@ -442,7 +442,7 @@ angular.module('scalearAngularApp')
             }
           })
         }
-      // })
+      })
 
       if( $scope.lecture.annotations  ){
         $scope.lecture.annotations.forEach(function(marker) {
@@ -1272,57 +1272,57 @@ angular.module('scalearAngularApp')
       $scope.check_peer_fuction_is_called = true
       var deferred = $q.defer()
       if ($scope.lecture) {
-        // Lecture.checkIfInDistancePeerSession({
-        //     course_id: $scope.lecture.course_id,
-        //     lecture_id: $scope.lecture.id
-        //   }).$promise
-        //   .then(function(response) {
-        //     if (response.distance_peer != "no_peer_session") {
-        //       $scope.distance_peer_session_id = response.distance_peer.distance_peer_id
-        //       startcheckIfDistancePeerIsAliveTimer()
+        Lecture.checkIfInDistancePeerSession({
+            course_id: $scope.lecture.course_id,
+            lecture_id: $scope.lecture.id
+          }).$promise
+          .then(function(response) {
+            if (response.distance_peer != "no_peer_session") {
+              $scope.distance_peer_session_id = response.distance_peer.distance_peer_id
+              startcheckIfDistancePeerIsAliveTimer()
 
-        //       $scope.name = response.name
-        //         // to prevent student seek after quiz, will use variable next_stop_time
-        //       $scope.distance_peer_status = response.user_distance_peer.status
-        //       var index = $scope.lecture.video_quizzes.map(function(x) {
-        //         return x.id; }).indexOf(response.distance_peer.online_quiz_id);
-        //       if ($scope.distance_peer_status == 1) { // = 1  stop = start_time_of_next_quiz
-        //         if ($scope.lecture.video_quizzes[index + 1]) {
-        //           $scope.next_stop_time = $scope.lecture.video_quizzes[index + 1].start_time
-        //         } else {
-        //           $scope.next_stop_time = $scope.total_duration
-        //         }
-        //       }
-        //       if (index == -1) { index = 0 };
-        //       if ($scope.lecture.video_quizzes[index]) {
-        //         if ($scope.distance_peer_status == 5) { // = 5  stop = quiz.end_time
-        //           $scope.next_stop_time = $scope.lecture.video_quizzes[index].end_time
-        //         } else if ([2, 3, 4].indexOf($scope.distance_peer_status) != -1) { // = 2,3,4  stop = quiz.time
-        //           $scope.next_stop_time = $scope.lecture.video_quizzes[index].time
+              $scope.name = response.name
+                // to prevent student seek after quiz, will use variable next_stop_time
+              $scope.distance_peer_status = response.user_distance_peer.status
+              var index = $scope.lecture.video_quizzes.map(function(x) {
+                return x.id; }).indexOf(response.distance_peer.online_quiz_id);
+              if ($scope.distance_peer_status == 1) { // = 1  stop = start_time_of_next_quiz
+                if ($scope.lecture.video_quizzes[index + 1]) {
+                  $scope.next_stop_time = $scope.lecture.video_quizzes[index + 1].start_time
+                } else {
+                  $scope.next_stop_time = $scope.total_duration
+                }
+              }
+              if (index == -1) { index = 0 };
+              if ($scope.lecture.video_quizzes[index]) {
+                if ($scope.distance_peer_status == 5) { // = 5  stop = quiz.end_time
+                  $scope.next_stop_time = $scope.lecture.video_quizzes[index].end_time
+                } else if ([2, 3, 4].indexOf($scope.distance_peer_status) != -1) { // = 2,3,4  stop = quiz.time
+                  $scope.next_stop_time = $scope.lecture.video_quizzes[index].time
 
-        //           if ([3, 4].indexOf($scope.distance_peer_status) != -1) {
-        //             showQuizOnline($scope.lecture.video_quizzes[index])
-        //             if ($scope.distance_peer_status == 3) {
-        //               var timer = $scope.lecture.video_quizzes[index].self
-        //             } else {
-        //               var timer = $scope.lecture.video_quizzes[index].in_group
-        //             }
-        //             setStatusTimer(timer)
-        //             startStatusTimer()
-        //           }
-        //         }
-        //         if (!($scope.lecture.video_quizzes[index].id in $scope.quiz_cue_distance_peer_list)) {
-        //           var quiz_cue = $scope.lecture_player.controls.cue($scope.next_stop_time, function() {
-        //             showQuizDistancePeer($scope.lecture.video_quizzes[index])
-        //           })
-        //           $scope.quiz_cue_distance_peer_list[$scope.lecture.video_quizzes[index].id] = quiz_cue.id
-        //         }
-        //       } else {
-        //         $scope.next_stop_time = $scope.total_duration
-        //       }
-        //     }
-        //     deferred.resolve()
-        //   });
+                  if ([3, 4].indexOf($scope.distance_peer_status) != -1) {
+                    showQuizOnline($scope.lecture.video_quizzes[index])
+                    if ($scope.distance_peer_status == 3) {
+                      var timer = $scope.lecture.video_quizzes[index].self
+                    } else {
+                      var timer = $scope.lecture.video_quizzes[index].in_group
+                    }
+                    setStatusTimer(timer)
+                    startStatusTimer()
+                  }
+                }
+                if (!($scope.lecture.video_quizzes[index].id in $scope.quiz_cue_distance_peer_list)) {
+                  var quiz_cue = $scope.lecture_player.controls.cue($scope.next_stop_time, function() {
+                    showQuizDistancePeer($scope.lecture.video_quizzes[index])
+                  })
+                  $scope.quiz_cue_distance_peer_list[$scope.lecture.video_quizzes[index].id] = quiz_cue.id
+                }
+              } else {
+                $scope.next_stop_time = $scope.total_duration
+              }
+            }
+            deferred.resolve()
+          });
       } else {
         deferred.resolve()
       }

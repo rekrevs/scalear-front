fc0818ebc7d7d0350ee168b54a9135c05aabad94
Ahmed Okasha <okashoon@gmail.com>
2018-03-28 18:20:34 +0200

    another fix to redirection from confirmation mail

--- app/scripts/controllers/users/confirmation/show.js
+++ app/scripts/controllers/users/confirmation/show.js
@@ -7,47 +7,47 @@ angular.module('scalearAngularApp')
     $scope.user = {}
     $scope.sending = true
    
-          $scope.sending = false;
-          if($stateParams['account_confirmation_success'] == "true"){
-            $scope.user_email = $stateParams.uid;
-            $modal.open({
-              template: '<style>.reveal-modal{height:auto;overflow: hidden;}</style>'+
-              "<center><span translate='account.account_confirmed' translate-values='{ email: user_email}'></span><span translate>global.please</span> <a class='tiny' ng-click='goTo()' translate>account.login</a> <span translate>account.start_using</span>ScalableLearning.<br><Small translate>account.go_to_support</small></center>",
-              scope: $scope,
-              controller:['$modalInstance', function($modalInstance) {                
-                $scope.goTo = function() {
-                  $state.go('login',{email: $scope.user_email})
-                  $modalInstance.dismiss('cancel');
-                }              
-              }]
-            })
+    $scope.sending = false;
+    if($stateParams['account_confirmation_success'] == "true"){
+      $scope.user_email = $stateParams.uid;
+      $modal.open({
+        template: '<style>.reveal-modal{height:auto;overflow: hidden;}</style>'+
+        "<center><span translate='account.account_confirmed' translate-values='{ email: user_email}'></span><span translate>global.please</span> <a class='tiny' ng-click='goTo()' translate>account.login</a> <span translate>account.start_using</span>ScalableLearning.<br><Small translate>account.go_to_support</small></center>",
+        scope: $scope,
+        controller:['$modalInstance', function($modalInstance) {                
+          $scope.goTo = function() {
+            $state.go('login',{email: $scope.user_email})
+            $modalInstance.dismiss('cancel');
+          }              
+        }]
+      })
+    }
+    else if($stateParams['account_confirmation_success'] == "invalid" || $stateParams['account_confirmation_success'] == "confirmed"){
+      $modal.open({
+        template: '<style>.reveal-modal{height:auto;overflow: hidden;}</style>'+
+        "<center><span translate>account.invalid_confirmation</span><br>"+
+        "<span translate>account.already_confirmation</span> <a class='tiny' ng-click='goTo()' translate>account.click_here</a> <span translate>global.to</span> <span translate>navigation.login</span>.<br>"+
+        "<span translate>account.new_confirmation</span> <a ui-sref='new_confirmation' ng-click='closeModal()' translate>account.click_here</a>.<br>"+
+        "<Small translate>account.go_to_support</small></center>",
+        scope: $scope,
+        controller:['$modalInstance', function($modalInstance) {                
+          $scope.goTo = function() {
+            $state.go('login',{email: $scope.user_email})
+            $modalInstance.dismiss('cancel');
           }
-          else if($stateParams['account_confirmation_success'] == "invalid"){
-            $modal.open({
-              template: '<style>.reveal-modal{height:auto;overflow: hidden;}</style>'+
-              "<center><span translate>account.invalid_confirmation</span><br>"+
-              "<span translate>account.already_confirmation</span> <a class='tiny' ng-click='goTo()' translate>account.click_here</a> <span translate>global.to</span> <span translate>navigation.login</span>.<br>"+
-              "<span translate>account.new_confirmation</span> <a ui-sref='new_confirmation' ng-click='closeModal()' translate>account.click_here</a>.<br>"+
-              "<Small translate>account.go_to_support</small></center>",
-              scope: $scope,
-              controller:['$modalInstance', function($modalInstance) {                
-                $scope.goTo = function() {
-                  $state.go('login',{email: $scope.user_email})
-                  $modalInstance.dismiss('cancel');
-                }
-                $scope.closeModal = function() {
-                  // $state.go('login',{email: $scope.user_email})
-                  $modalInstance.dismiss('cancel');
-                }
-              }]
-            })
+          $scope.closeModal = function() {
+            // $state.go('login',{email: $scope.user_email})
+            $modalInstance.dismiss('cancel');
           }
-          else if($stateParams['account_confirmation_success'] == "confirmed"){
-            // UserSession.allowRefetchOfUser()
-            $timeout(function() {
-              $state.go("confirmed")
-            }, 2500)
-          }
+        }]
+      })
+    }
+    else {
+      Token.setToken($stateParams);
+      $timeout(function() {
+        $state.go("course_list")
+      }, 1000)            
+    }
         
     // })
 

8f15460d58c59fe9dfa7d1a82ff9a5375b33685b
poussy <poussy@novelari.com>
2018-04-04 12:58:34 +0200

    saving new line in the student notes in editable mode and in timline

--- app/scripts/directives/student/courseware/lecture_middle_directives.js
+++ app/scripts/directives/student/courseware/lecture_middle_directives.js
@@ -218,8 +218,8 @@ angular
       var fontSize = "14px"
       if (MobileDetector.isMobile()){
         fontSize = "10px";
-      } 
-        
+      }
+
       return {
         restrict: "E",
         template:
@@ -721,7 +721,7 @@ angular
         template:
           '<div   e-rich-textarea onshow="moveCursorToEnd()" e-rows="3" e-cols="100" blur="submit" editable-textarea="value" e-form="myform" buttons="no" onaftersave="saveData()" e-placeholder="Note..." ng-click="show()" e-style="width:95% !important; font-size: 13px;color: teal; height:80px" style="padding:0 9px">' +
           '<div class="note" style="word-break: break-word; margin: 0px;cursor: text;float:left">' +
-          '<span ng-bind-html="value"></span>' +
+          '<span ng-bind-html="value" style="white-space: pre-wrap;"></span>' +
           "</div>" +
           '<div style="font-size: 10px; float: right; display: inline-block;">' +
           '<delete_button size="small" action="delete()" vertical="false" text="false" ></delete_button>' +
@@ -740,13 +740,6 @@ angular
           });
 
           scope.moveCursorToEnd = function() {
-            // $timeout(function() {
-            //     var textarea = $('.editable-input');
-            //     var strLength= textarea.val().length;
-            //     textarea.focus();
-            //     textarea[0].setSelectionRange(strLength, strLength);
-            // });
-
             shortcut.add(
               "enter",
               function() {
@@ -755,8 +748,17 @@ angular
               { disable_in_input: false }
             );
             shortcut.add(
+                "shift+enter",
+                function() {
+                 $('div.editable-input>p.medium-editor-p').html('<br>'+$('div.editable-input>p.medium-editor-p').html());
+                 $('div.ng-scope editable>span.ng-binding>p.medium-editor-p').html('<br>'+$('div.note>span.ng-binding>p.medium-editor-p').html());
+                },
+                { disable_in_input: true }
+              );
+            shortcut.add(
               "esc",
               function() {
+                console.log("i m in escape");
                 $("form.editable-textarea").submit();
               },
               { disable_in_input: false }
@@ -842,7 +844,7 @@ angular
         },
         templateUrl: "/views/student/lectures/dynamic_annotation_student.html",
         link: function(scope, element, attrs) {
-         
+
           scope.closeBtn = scope.close();
           scope.actionBtn = scope.action();
         }

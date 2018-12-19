a3ece24ea191b63749c58a558681d199a0c31ad6
poussy <poussy@novelari.com>
2018-12-19 13:00:28 +0200

    merge conflict fix

--- app/scripts/services/kpi.js
+++ app/scripts/services/kpi.js
@@ -6,7 +6,6 @@ angular.module('scalearAngularApp')
     $http.defaults.useXDomain = true;
     return $resource(scalear_api.host + '/:lang/kpis/:document_id/:action', { lang: $translate.use() },
       {
-<<<<<<< HEAD
         'readData': { method: 'GET', headers: headers, params: { action: 'read_data' } }, // Not DONE
         'readTotals': { method: 'GET', headers: headers, params: { action: 'read_totals' } }, // Not DONE
         'readSeries': { method: 'GET', headers: headers, params: { action: 'read_series' } }, // Not DONE
@@ -14,15 +13,7 @@ angular.module('scalearAngularApp')
         'getReportDataCourseDuration': { method: 'POST', headers: headers, params: { action: 'get_report_data_course_duration' } },
         'exportSchoolStatistics': { method: 'GET', headers: headers, params: { action: 'export_school_statistics' } }, // Not DONE
         'getAllYoutubeData': { method: 'GET', headers: headers, params: { action: 'get_all_youtube_data' } },
-        'getAllYoutubeVideoUrls': { method: 'GET', headers: headers, params: { action: 'get_all_youtube_urls' }, isArray: true }
-=======
-        'readData':{method: 'GET', headers:headers, params: {action: 'read_data'} }, // Not DONE
-        'readTotals':{method: 'GET', headers:headers, params: {action: 'read_totals'} }, // Not DONE
-        'readSeries':{method: 'GET', headers:headers, params: {action: 'read_series'} }, // Not DONE
-        'readTotalsForDuration':{method: 'GET', headers:headers, params: {action: 'read_totals_for_duration'} },
-        'getReportDataCourseDuration':{method: 'POST', headers:headers, params: {action: 'get_report_data_course_duration'} },
-        'exportSchoolStatistics':{method:'GET',headers:headers, params: {action: 'export_school_statistics'}}, // Not DONE
+        'getAllYoutubeVideoUrls': { method: 'GET', headers: headers, params: { action: 'get_all_youtube_urls' }, isArray: true },
         'getAllCoursesIds':{method:'GET',headers:headers, params: {action: 'get_all_courses_ids'},isArray:true} 
->>>>>>> develop
       });
   }])

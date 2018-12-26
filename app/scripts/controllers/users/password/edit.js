4bfc3a04c403e8584f7fcc099f6914ab949de580
poussy <poussy@novelari.com>
2018-12-26 13:57:38 +0200

    remove log

--- app/scripts/controllers/users/password/edit.js
+++ app/scripts/controllers/users/password/edit.js
@@ -14,7 +14,7 @@ angular.module('scalearAngularApp')
             Token.setToken($scope.user)
             delete $scope.user.errors;
             User.change_password({}, $scope.user,
-                function (resp, headers) { console.log(resp)
+                function (resp, headers) { 
                     User.getCurrentUser({},function(){
                         $state.go("dashboard");
                     });

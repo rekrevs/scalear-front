e42e393ccceee95b1a4800f870aeb0c615823004
poussy <poussy@novelari.com>
2019-05-26 13:17:25 +0200

    lenting

--- app/scripts/services/vimeo_upload_utils.js
+++ app/scripts/services/vimeo_upload_utils.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('scalearAngularApp')
-    .factory('VimeoUtils', ['$resource','scalear_api','headers','$http','$stateParams','$translate',function ($resource,scalear_api,headers,$http,$stateParams,$translate) {
+    .factory('VimeoUtils', ['$resource', 'scalear_api', 'headers', '$http', '$stateParams', '$translate', function ($resource, scalear_api, headers, $http, $stateParams, $translate) {
         $http.defaults.useXDomain = true;
-        return $resource(scalear_api.host + '/:lang/vimeo_uploads/:action', {  },{
+        return $resource(scalear_api.host + '/:lang/vimeo_uploads/:action', {}, {
             "updateVimeoUploads": { method: 'POST', ignoreLoadingBar: true, params: { action: 'update_vimeo_table' }, headers: headers },
             "generateVimeoUploadDetails": { method: 'GET', ignoreLoadingBar: true, params: { action: 'get_vimeo_upload_details' }, headers: headers },
             "deleteUploadedVimeoVideo": { method: 'DELETE', params: { action: 'delete_vimeo_video_angular' }, headers: headers },
@@ -13,17 +13,17 @@ angular.module('scalearAngularApp')
             "updateVimeoUploadedVideoData": { method: 'POST', params: { action: 'update_vimeo_video_data' }, headers: headers }
         });
 
-    }]).factory("VimeoModel", ['VimeoUtils',function (VimeoUtils) {
+    }]).factory("VimeoModel", ['VimeoUtils', function (VimeoUtils) {
         var service = {}
-        service.deleteVideo=function(vimeo_video_id,lec_id) {
+        service.deleteVideo = function (vimeo_video_id, lec_id) {
             return VimeoUtils.deleteUploadedVimeoVideo({
                 vimeo_vid_id: vimeo_video_id,
                 lecture_id: lec_id
-            }, { })
+            }, {})
                 .$promise
         }
 
-        service.deleteVimeoUploadLink=function(link) {
+        service.deleteVimeoUploadLink = function (link) {
             return VimeoUtils.deleteUploadLink({
                 link: link
             }, {})
@@ -41,7 +41,7 @@ angular.module('scalearAngularApp')
                 .$promise
         }
 
-        service.updateVimeoVideoData=function (video_id, data){
+        service.updateVimeoVideoData = function (video_id, data) {
             return VimeoUtils.updateVimeoUploadedVideoData({
                 video_id: video_id,
                 name: data.name
@@ -49,7 +49,7 @@ angular.module('scalearAngularApp')
                 .$promise
         }
 
-        service.getVimeoVideoId=function (lecture_id) {
+        service.getVimeoVideoId = function (lecture_id) {
             return VimeoUtils.getVimeoVideoId({
                 id: lecture_id
             }, {})
@@ -59,17 +59,17 @@ angular.module('scalearAngularApp')
                 })
         }
 
-        service.getVimeoUploadingStatus=function (lecture_id) {
+        service.getVimeoUploadingStatus = function (lecture_id) {
             return VimeoUtils.getUploadingStatus({
                 id: lecture_id
-            }, {id: lecture_id})
+            }, { id: lecture_id })
                 .$promise
                 .then(function (data) {
                     return data.status
                 })
         }
 
-         service.getVimeoUploadDetails=function() {
+        service.getVimeoUploadDetails = function () {
             return VimeoUtils.generateVimeoUploadDetails({
             }, {})
                 .$promise
@@ -77,8 +77,6 @@ angular.module('scalearAngularApp')
                     return data.details
                 })
         }
-        
-        return service
-    }
 
-    ])
+        return service
+    }])

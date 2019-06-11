e5b39d061d98c6e65080f434fe39d84c511c619d
poussy <poussy@novelari.com>
2019-06-11 13:16:02 +0200

    wip

--- referenceConf-dev.js
+++ referenceConf-dev.js
@@ -151,6 +151,7 @@ exports.config = {
     // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
 
     // The location of the selenium standalone server .jar file.
+  
     seleniumServerJar: null, //'selenium/selenium-server-standalone-2.39.0.jar',
     // The port to start the selenium server on, or null if the server should
     // find its own unused port.

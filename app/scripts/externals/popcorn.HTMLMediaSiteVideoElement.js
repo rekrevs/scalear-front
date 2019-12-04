60432300a30e9b67a10f9262a04e8240dad1f85b
poussy <poussy@novelari.com>
2019-12-04 12:19:20 +0200

    change the circle to rectangle of mediasite note

--- app/scripts/externals/popcorn.HTMLMediaSiteVideoElement.js
+++ app/scripts/externals/popcorn.HTMLMediaSiteVideoElement.js
@@ -331,7 +331,7 @@
 
     function appendPlayButton(){ 
       var play_button_container = document.createElement('DIV')
-      play_button_container.className = 'media_site_play_button_container media_site_click_button_small_font'
+      play_button_container.className = 'button small'
       play_button_container.id = 'clickMe'
       play_button_container.innerHTML = "Click to Load Video";
       parent.appendChild(play_button_container);
